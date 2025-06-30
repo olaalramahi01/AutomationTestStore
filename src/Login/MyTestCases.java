4a97d43dd3318f4d8c0d020c1ed10c209f09a44c
olaalramahi01 <olaalramahi01@gmail.com>
2025-06-30 21:04:06 +0300

    update

---
+++ src/Login/MyTestCases.java
@@ -0,0 +1,34 @@
+package Login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class MyTestCases {
+	WebDriver driver = new ChromeDriver(); 
+	 String theURL = "https://automationteststore.com/";
+	 String SignupPage = "https://automationteststore.com/index.php?rt=account/create"; 
+	 
+	 @BeforeTest
+	 public void mySetup() {
+	  driver.get(theURL);
+	  
+	  driver.manage().window().maximize();
+	  
+	 }
+	 
+	 
+	 @Test(priority = 1)
+	 public void Signup() {
+	  
+	  driver.navigate().to(SignupPage);
+	  
+	  WebElement firstName = driver.findElement(By.xpath("//*[@id=\"AccountFrm_firstname\"]"));
+	  
+	  firstName.sendKeys("ahmad");
+	  
+	 }
+}
